c2d7e148a8344a08f00a7fd55e32694ce8e07565
salman2038 <m.salman25900@gmail.com>
2025-11-03 14:50:15 +0500

    update app.py full code with gemini

--- app.py
+++ app.py
@@ -781,4 +781,4 @@ def reset_db():
 #     app.run(debug=True)
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)    
+    serve(app, host="0.0.0.0", port=8080)
